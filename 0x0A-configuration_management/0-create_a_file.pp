48ed04e9794afab5a346e4755fd3fa612202db37
Miko1203744 <106764762+Miko1203744@users.noreply.github.com>
2023-01-11 20:40:37 -0600

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,7 @@
-# file in tmp holberton
-file { '/tmp/holberton':
-ensure  => file,
-  path  => '/tmp/holberton',
-  mode  => '0744',
-  owner => 'www-data',
-  group => 'www-data',
-content => 'I love Puppet'
+# Creates a file
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
